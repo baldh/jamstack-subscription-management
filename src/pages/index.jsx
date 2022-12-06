84fe145ad1a5f0f641bb2ba6769368b659f59753
= <=>
2022-12-06 23:37:26 +0530

    Another dumb commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,11 @@
 import React,{useState, useEffect} from "react"
 export default function IndexPage() {
-  console.log("Child renders")
   const [netlifyIdentity, setNetlifyIdentity] = useState(undefined)
 
   useEffect(()=> {
     setNetlifyIdentity(window.netlifyIdentity)
   },[])
 
-  useEffect(()=> {
-      if(netlifyIdentity !== undefined) console.log("netlifyIdentity has changed")
-  }, [netlifyIdentity])
-
-
   if(netlifyIdentity === undefined) return <button>Loading...</button>
-  return <button onClick={()=> console.log(netlifyIdentity)}>Log Data</button>
+  return <button onClick={()=> netlifyIdentity.open()}>Log Data</button>
 }
--- src/templates/site/site-context.jsx
+++
@@ -1,8 +0,0 @@
-import React,{createContext} from 'react'
-
-export const SiteContext = createContext()
-
-export default function SiteContextProvider({children}) {
-  const [theme, setTheme] = React.useState("light")
-  const [netlifyIdentity, setNetlifyIdentity] = React.useState("")
-}
